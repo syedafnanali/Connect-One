f501c51942abbd2bbfb82e5e5705f0b95583510f
Syed Afnan Ali <43354238+syedafnanali@users.noreply.github.com>
2019-03-21 14:58:27 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports={
-    mongoURI :"mongodb+srv://syed:syed@connect-one-qnk4g.mongodb.net/test?retryWrites=true",
+    mongoURI :"mongodb+srv://syed:@connect-one-qnk4g.mongodb.net/test?retryWrites=true",
     secretOrkey:"secret123."
 
 }
